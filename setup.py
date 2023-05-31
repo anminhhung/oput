161cd8f6e2f0be66d4b8cadaa084e37d46758991
anminhhung <anminhhung99@gmail.com>
2023-05-31 16:40:03 +0700

    update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,16 @@
 import setuptools
+from setuptools import setup
+
+install_requires = [
+    'torch>=1.10.0',
+    'pytorch_ranger>=0.1.1',
+]
 
 setuptools.setup(
     name="oput",
+    description=('pytorch-optimizer'),
     version='0.1.1',
     packages=setuptools.find_packages(),
+    install_requires=install_requires,
     python_requires='>=3.6',
 )
